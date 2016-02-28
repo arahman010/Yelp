9126fb2c43b23046e025ac62faa6d24650bf55e5
Azizur Rahman <Sarzil@Azizurs-MacBook-Pro.local>
2016-02-28 00:55:42 -0500

    All requirements are done and optional 1 is half done

--- Yelp/BusinessesViewController.swift
+++ Yelp/BusinessesViewController.swift
@@ -8,41 +8,71 @@
 
 import UIKit
 
-class BusinessesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class BusinessesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate, UIScrollViewDelegate {
 
     @IBOutlet weak var tableView: UITableView!
     var businesses: [Business]!
     
+    let searchBar = UISearchBar()
+    
+    var data: [Business]! = []
+    
+    var filteredData : [Business]! = []
+    
+    var isMoreDataLoading = false
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        searchBar.tintColor = UIColor.whiteColor()
+        searchBar.backgroundColor = UIColor.blackColor()
+        searchBar.sizeToFit()
+        navigationItem.titleView = searchBar
+        
+        
+     
+        searchBar.delegate = self
+        
+        
         
         tableView.dataSource = self
         tableView.delegate = self
         tableView.rowHeight = UITableViewAutomaticDimension
         tableView.estimatedRowHeight = 120
 
+/*
         Business.searchWithTerm("Thai", completion: { (businesses: [Business]!, error: NSError!) -> Void in
             self.businesses = businesses
             
-            self.tableView.reloadData()
+            
         
             for business in businesses {
+                self.data.append(business)
                 print(business.name!)
                 print(business.address!)
             }
+            self.filteredData = self.data
+            self.tableView.reloadData()
         })
-
-/* Example of Yelp search with more search options specified
+*/
+// Example of Yelp search with more search options specified
         Business.searchWithTerm("Restaurants", sort: .Distance, categories: ["asianfusion", "burgers"], deals: true) { (businesses: [Business]!, error: NSError!) -> Void in
             self.businesses = businesses
             
+            
+            
             for business in businesses {
+                self.data.append(business)
                 print(business.name!)
                 print(business.address!)
             }
+            self.filteredData = self.data
+            self.tableView.reloadData()
         }
-*/
+// ends here
     }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -50,8 +80,8 @@ class BusinessesViewController: UIViewController, UITableViewDataSource, UITable
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if businesses != nil {
-            return businesses.count
+        if filteredData != nil {
+            return filteredData.count
         }
         else {
             return 0
@@ -61,9 +91,86 @@ class BusinessesViewController: UIViewController, UITableViewDataSource, UITable
   
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("BusinessCell", forIndexPath: indexPath) as! BusinessCell
-        cell.business = businesses[indexPath.row]
+        cell.business = filteredData[indexPath.row]
         return cell
     }
+    
+    
+    // For Searching
+    
+    
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        // When there is no text, filteredData is the same as the original data
+        if searchText.isEmpty {
+            filteredData = data
+        } else {
+            // The user has entered text into the search box
+            // Use the filter method to iterate over all items in the data array
+            // For each item, return true if the item should be included and false if the
+            // item should NOT be included
+            filteredData = data.filter({(dataItem: Business) -> Bool in
+                // If dataItem matches the searchText, return true to include it
+                if dataItem.name!.rangeOfString(searchText, options: .CaseInsensitiveSearch) != nil {
+                    return true
+                } else {
+                    return false
+                }
+            })
+        }
+        tableView.reloadData()
+    }
+    
+    func searchBarTextDidBeginEditing(searchBar: UISearchBar) {
+        self.searchBar.showsCancelButton = true
+    }
+    
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        searchBar.showsCancelButton = false
+        searchBar.text = ""
+        searchBar.resignFirstResponder()
+    }
+     // Infinite Scrolling
+ 
+    func loadMoreData() {
+        
+        // ... Create the NSURLRequest (myRequest) ...
+        Business.searchWithTerm("Restaurants", sort: .Distance, categories: ["asianfusion", "burgers"], deals: true) { (businesses: [Business]!, error: NSError!) -> Void in
+            
+            self.filteredData.appendContentsOf(businesses)
+            
+            for business in businesses {
+                self.data.append(business)
+                print(business.name!)
+                print(business.address!)
+            }
+            self.filteredData = self.data
+            
+       
+                
+                // Update flag
+                self.isMoreDataLoading = false
+            self.tableView.reloadData()
+        }
+    }
+    
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        if (!isMoreDataLoading) {
+            // Calculate the position of one screen length before the bottom of the results
+            let scrollViewContentHeight = tableView.contentSize.height
+            let scrollOffsetThreshold = scrollViewContentHeight - tableView.bounds.size.height
+            
+            // When the user has scrolled past the threshold, start requesting
+            if(scrollView.contentOffset.y > scrollOffsetThreshold && tableView.dragging) {
+                
+                isMoreDataLoading = true
+                
+                // Code to load more results
+                loadMoreData()
+            }
+        }
+    }
+    
+   
     /*
     // MARK: - Navigation
 
